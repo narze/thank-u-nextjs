dc2a7ce70933d18cb1ad51c8f6ac3410bbe0af68
Manassarn Manoonchai <manassarn@gmail.com>
2020-09-10 19:47:46 +0700

    :recycle: Wrap test render with ThemeProvider

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,28 +1,15 @@
 import React from 'react'
 import { render } from '../testUtils'
 import { Home } from '../../pages/index'
-import { ThemeProvider, theme } from '@chakra-ui/core'
-
-const renderWithProvider = (
-  ui,
-  { providerProps = {}, ...renderOptions } = {}
-) => {
-  return render(
-    <ThemeProvider theme={theme} {...providerProps}>
-      {ui}
-    </ThemeProvider>,
-    renderOptions
-  )
-}
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = renderWithProvider(<Home />, {})
+    const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders with buttons alert', () => {
-    const { getByText } = renderWithProvider(<Home />, {})
+    const { getByText } = render(<Home />, {})
 
     getByText('Emotion CSS')
     getByText('Emotion React')
