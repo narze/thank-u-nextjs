c270eb71b7a50a06280b73f348e74d6ba4893f31
Manassarn Manoonchai <manassarn@gmail.com>
2020-09-10 17:50:06 +0700

    :white_check_mark: Fix tests

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
+import { render } from '../testUtils'
 import { Home } from '../../pages/index'
+import { ThemeProvider, theme } from '@chakra-ui/core'
+
+const renderWithProvider = (
+  ui,
+  { providerProps = {}, ...renderOptions } = {}
+) => {
+  return render(
+    <ThemeProvider theme={theme} {...providerProps}>
+      {ui}
+    </ThemeProvider>,
+    renderOptions
+  )
+}
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
+    const { asFragment } = renderWithProvider(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+  it('renders with buttons alert', () => {
+    const { getByText } = renderWithProvider(<Home />, {})
+
+    getByText('Emotion CSS')
+    getByText('Emotion React')
+    getByText('Chakra-UI')
   })
 })
